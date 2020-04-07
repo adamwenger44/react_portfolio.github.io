33330ac636b514ea342d5ce177adf096f5aa30a6
Adam Wenger <adamwenger44@gmail.com>
2020-04-07 16:18:08 -0400

    updated contact me page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -97,18 +97,8 @@ const portfolio = () => {
 
                             </div>
                         </div>
-                        <br></br>
-                        <div className="gitProfile">
-                            <p>GitHub Profile: <a href="https://github.com/adamwenger44">
-                                https://github.com/adamwenger44</a> </p>
-                            <p>Linkedin Profile: <a href="https://www.linkedin.com/in/adam-wenger-25a73b129/"
-                            >https://www.linkedin.com/in/adam-wenger-25a73b129/</a></p>
-                            <p>Email Address: adamwenger44@gmail.com</p>
-                            <p>Phone Number:(704)-430-8647</p>
-                            <a href="https://github.com/adamwenger44/portfolio_bootstrap.github.io/blob/master/resume.pdf"
-                            > Resume</a>
-
-                        </div>
+                   
+                    
                     </div>
 
                 </div>
