ad72196667dca27490b597df32b124e44420034c
Adam Wenger <adamwenger44@gmail.com>
2020-04-07 16:01:01 -0400

    updated about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,28 +24,19 @@ const mainPage = () => {
 
                                     <p>My name is Adam Wenger. I am 21 years old and am currently taking classes at UNCC
                                                 for a web
-                                                developement bootcamp. I went to a 4 year college but wasn't putting in my best
-                                                effort,
+                                                developement bootcamp. I went to a 4 year college but decided it was not the right path for me,
                                                 partially for not knowing what I wanted to study or do for the rest of my life.
                                                 This time around
-                                                it will be different, I have enjoyed coding very much so far! I have a few
+                                                is different, I have enjoyed coding very much so far and can see myself pursing a career in the field! I have a few
                                                 hobbies outside of
                                                 school. One is playing video games, I've been playing video games my entire life
                                                 and has always
-                                                been a great way to relax after a long day. My favorite games are shooters like
-                                Call of Duty.</p>
+                                                been a great way to relax after a long day.  A dream job for me would be to develop video games. </p>
                                 </div>
                             </div>
+
                             <p>
-                                On the more active side I LOVE bowling! I used to work at a bowling alley so that really started
-                                my passion for it. I would bowl before and after work and on days off. I was there every single
-                                day, I wish I could bowl half as much as I used to. Unfortunately it's a bit expensive, and I
-                                dont have the time anymore. And last and my most important part of my life is my son Braden, He
-                                is 1 year old and is so cute! I love playing with him and making him laugh, he is the best.
-                    </p>
-                            <p>
-                                Once I complete this program I hope to get a good job and be able to provide a good life for my
-                                family.
+                                Once I complete this program I hope to get a good job and be able to provide a good life for myself and my son
                     </p>
 
                         </blockquote>
